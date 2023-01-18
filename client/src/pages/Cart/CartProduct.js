a19c46e11b4255f8a250fc48b95021a83393aacf
AbdullahWins <noyononline@gmail.com>
2023-01-18 17:47:49 +0600

    modified cart

--- client/src/pages/Cart/CartProduct.js
+++ client/src/pages/Cart/CartProduct.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useState } from "react";
 
 const CartProduct = ({ product }) => {
   const [quantity, setQuantity] = useState(product.quantity);
