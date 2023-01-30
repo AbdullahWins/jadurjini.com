50b0a433c351c165e1d1560922667a2237a3690a
AbdullahWins <noyononline@gmail.com>
2023-01-30 14:43:36 +0600

    added product details

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -27,6 +27,7 @@ const Home = () => {
 
   const [products, setProducts] = useState([]);
 
+  console.log(products);
   useEffect(() => {
     const url = `${process.env.REACT_APP_baseURL}/testProducts`;
     fetch(url)
@@ -48,16 +49,18 @@ const Home = () => {
         </div>
       </section>
       <section>
+        <hr />
         <h2 className="text-xl pl-2 font-bold md:text-center md:text-4xl md:my-6">
           Explore by category
         </h2>
+        <hr />
         <div className="flex flex-row overflow-auto over p-2 md:justify-center md:gap-3">
           {categorySlider.map((slider, i) => (
             <Link key={i} to={`/category/${slider.categoryName}`}>
-              <div className="flex flex-col items-center justify-center text-center carousel-item bg-gray-400 rounded-md p-4 m-2 h-10 w-10 md:h-32 md:w-32">
+              <div className="flex flex-col items-center justify-center text-center carousel-item bg-green-100 rounded-md p-4 m-2 h-10 w-10 md:h-32 md:w-32">
                 <img
                   src={slider.categoryImg}
-                  className="rounded-box m-0 mt-3 h-8 w-8 md:h-44 md:w-44"
+                  className="rounded-box m-0 mt-3 h-8 w-8 md:h-24 md:w-24"
                   alt=""
                 />
                 <p className="m-0 mb-2 md:text-2xl">{slider.categoryName}</p>
@@ -67,27 +70,50 @@ const Home = () => {
         </div>
       </section>
       <section>
+        <hr />
         <h2 className="text-xl pl-2 font-bold md:text-center md:text-4xl md:my-6">
           Order Again
         </h2>
-        <div className="flex flex-row overflow-auto md:grid md:grid-cols-3 lg:grid-cols-4 w-full p-2 gap-4 h-22 md:h-96">
+        <hr />
+        <div className="flex flex-row overflow-auto  lg:grid-cols-4 h-22 w-full p-2 gap-4">
           {products.map((product, i) => (
             <div key={i} className="w-1/2 md:w-full">
               <Link to={`/products/${product._id}`}>
-                <div className="card card-side bg-slate-200">
+                <div className="card card-side bg-slate-200 rounded-md">
                   <figure>
                     <img
-                      className="w-20 h-16 md:w-40 md:h-20"
+                      className="w-20 h-20 md:w-40 md:h-40"
                       src={product.productImage}
                       alt="food"
                     />
                   </figure>
-                  <div className="card-body p-0 px-2 flex flex-col justify-center md:w-40">
-                    <span className="card-title text-xs">
-                      {product.productName}
-                    </span>
-                    <span className="text-xs">{product.productPrice}</span>
-                    <span className="text-xs">{product.time}</span>
+                  <div className="card-body p-2 flex flex-col justify-around w-32 md:w-40">
+                    <div className="flex items-center justify-between">
+                      <span className="card-title text-xs md:text-sm">
+                        {product.productName}
+                      </span>
+                      <span className="font-bold bg-green-400 text-xs md:text-sm px-2 rounded-full">
+                        {product?.productRating}
+                        <i class="fa-solid fa-star text-xs md:text-sm"></i>
+                      </span>
+                    </div>
+                    <div className="flex items-center justify-between">
+                      <span className="text-xs md:text-lg font-bold">
+                        Price:
+                      </span>
+                      <span className="font-bold bg-red-200 px-2 text-xs md:text-lg rounded-full">
+                        {product.productPrice} tk
+                      </span>
+                    </div>
+                    <div className="flex items-center justify-between">
+                      <span className="card-title text-gray-400 text-xs md:text-sm">
+                        {product.shopName}
+                      </span>
+                      <span className=" flex items-center font-bold bg-blue-400 px-1 text-gray-100 rounded-full text-xs md:text-lg">
+                        <i class="fa-regular fa-circle-check text-xs md:text-lg"></i>
+                        <span className="hidden md:block text-xs md:text-sm">Verified</span>
+                      </span>
+                    </div>
                   </div>
                 </div>
               </Link>
@@ -96,9 +122,11 @@ const Home = () => {
         </div>
       </section>
       <section>
+        <hr />
         <h2 className="text-xl pl-2 font-bold md:text-center md:text-4xl md:my-6">
           Offers
         </h2>
+        <hr />
         <div className="carousel carousel-center w-full p-2 space-x-2 h-36 md:h-96">
           {bannerSlider.map((slider, i) => (
             <div key={i} className="carousel-item w-full">
@@ -108,17 +136,19 @@ const Home = () => {
         </div>
       </section>
       <section>
+        <hr />
         <h2 className="text-xl pl-2 font-bold md:text-center md:text-4xl md:my-6">
           Recommendations
         </h2>
+        <hr />
         <div
           className=" grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 w-full  md:grid-cols-
-         gap-4 p-2"
+         gap-12 p-2"
         >
-          {products.map((product, i) => (
+          {products?.map((product, i) => (
             <Link key={i} to={`/products/${product._id}`}>
               <div>
-                <div className="card w-full bg-base-100 shadow-xl">
+                <div className="card w-52 shadow-xl bg-gray-100 border-solid border-2 border-gray-200">
                   <figure>
                     <img
                       className="h-32 w-48 md:h-44 md:w-full"
@@ -126,10 +156,27 @@ const Home = () => {
                       alt="foods again"
                     />
                   </figure>
-                  <div className="card-body p-2">
-                    <p className="font-bold">{product.productName}</p>
-                    <p>BDT {product.productPrice}</p>
-                    <p>{product.time}</p>
+                  <div className="card-body p-3">
+                    <div className="flex items-center justify-around">
+                      <p className="font-bold">{product?.productName}</p>
+                      <span className="font-bold bg-green-400 px-2 rounded-full">
+                        {product?.productRating}
+                        <i class="fa-solid fa-star text-sm"></i>
+                      </span>
+                    </div>
+                    <p className="text-xs text-gray-400 m-0">
+                      {product?.productCategory}
+                    </p>
+                    <p className="font-bold">BDT: {product?.productPrice} tk</p>
+                  </div>
+                  <div className="flex items-center justify-between">
+                    <p className="text-lg text-gray-400 p-2">
+                      {product?.shopName}
+                    </p>
+                    <span className="font-bold bg-blue-400 px-1 mr-2 text-gray-100 rounded-full">
+                      <i class="fa-regular fa-circle-check"></i>
+                      Verified
+                    </span>
                   </div>
                 </div>
               </div>
